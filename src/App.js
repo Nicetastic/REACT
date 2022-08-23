ca1e2e911ed104da310844f46ec478178a60d405
Nicetastic <urielscagliarcc@gmail.com>
2022-08-23 03:33:08 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+let gif = "simpsons-homer.gif";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={gif} className="App-logo" alt="logo" />
+        <h1>Primer proyecto en React</h1>
+        <h2><u>Alumno:</u> Scaglia Uriel</h2>
     </div>
   );
 }
