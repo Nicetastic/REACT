f89a6e9be103bd2516e6cae76c21f8e6d4ceaa9f
Nicetastic <urielscagliarcc@gmail.com>
2022-09-05 19:56:21 -0300

    Contador

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import "./ItemListContainer.css"
 import Titulo from '../Titulo/Titulo'
+import ItemCount from '../ItemCount/ItemCount'
 
-const ItemListContainer = () => {
+const ItemListContainer = ({texto}) => {
+  const onAdd = (cantidad) => {
+    console.log(`Agregaste ${cantidad} unidades`)
+  }
+
   return (
     <div>
-      <Titulo greeting= "Saludo"/>
+      <Titulo greeting={texto} />
+      <ItemCount initial={1} stock={7} onAdd={onAdd} />
     </div>
   )
 }
