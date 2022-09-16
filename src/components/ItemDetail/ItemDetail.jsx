2c298e6c6bbdc326ba4a70f2f7a8adee0bbb8785
Nicetastic <urielscagliarcc@gmail.com>
2022-09-15 21:19:31 -0300

    Pre entrega proyecto final

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({item}) => {
+
+  const onAdd = (cantidad) => {
+    console.log(`Agregaste ${cantidad} unidades`)
+  }
+  
   return (
     <div className='ContainerItemDetail'>
         <div className='detail'>
@@ -10,6 +16,7 @@ const ItemDetail = ({item}) => {
                 <h1>{item.title}</h1>
                 <h4>{item.price}</h4>
                 <p>{item.desc}</p>
+                <ItemCount initial={1} stock={7} onAdd={onAdd} />
             </div>
         </div>
     </div>
